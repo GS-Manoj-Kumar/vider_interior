a6a6852a93fd475157632d8f3d6890a4e9540743
GS-Manoj-Kumar <gatakari65@gmail.com>
2026-06-11 23:09:52 +0530

    add sitemap, robots.txt, meta tags, conversion tracking

--- config/urls.py
+++ config/urls.py
@@ -15,17 +15,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path , include
-from django.conf.urls import handler404
+from django.urls import path, include
+from django.contrib.sitemaps.views import sitemap
+from django.views.generic import TemplateView
+from pages.sitemaps import StaticViewSitemap
 from pages.views import custom_404
 
+sitemaps = {'static': StaticViewSitemap}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
     path('leads/', include('leads.urls')),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    path('robots.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
 ]
-
-
 
 handler404 = 'pages.views.custom_404'
